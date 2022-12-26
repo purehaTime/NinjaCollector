45c89df8f44a500d0f1b9fe02f2593eb9415f3da
Stanislav <akimov.cta@gmail.com>
2022-12-26 18:43:51 +0300

    Add log service

--- LoggerService/Program.cs
+++ LoggerService/Program.cs
@@ -5,10 +5,13 @@ namespace LoggerService
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            //app.MapGet("/", () => "Hello World!");
 
+            app.MapControllers();
             app.Run();
         }
     }
---
+++ LoggerService/Services/LogService.cs
@@ -0,0 +1,17 @@
+﻿using Grpc.Core;
+using LoggerService.Protos;
+
+namespace LoggerService.Services
+{
+    public class LogService : Logger.LoggerBase
+    {
+        public override Task<WriteResponse> WriteLog(LogModel request, ServerCallContext context)
+        {
+
+
+
+
+            return Task.FromResult(new WriteResponse { Success = true });
+        }
+    }
+}
