ee67b6ac087fda332cafd1aeb7e7eeb59eba4b85
Stanislav <akimov.cta@gmail.com>
2023-01-06 21:59:20 +0300

    Add base realization for mongoDb

--- DbService/Interfaces/IRepository.cs
+++ DbService/Interfaces/IRepository.cs
@@ -6,14 +6,14 @@ namespace DbService.Interfaces
         where TEntity : class
     {
         Task<TEntity> Find (FilterDefinition<TEntity> filter, FindOptions options, CancellationToken cToken);
-        Task<TEntity> Insert(TEntity entity, InsertOneOptions options, CancellationToken cToken);
-        Task<TEntity> Update (FilterDefinition<TEntity> filter, UpdateDefinition<TEntity> update, UpdateOptions options, CancellationToken cToken);
-        Task<TEntity> Delete(FilterDefinition<TEntity> filter, DeleteOptions options, CancellationToken cToken);
+        Task<bool> Insert(TEntity entity, InsertOneOptions options, CancellationToken cToken);
+        Task<TEntity> Update (FilterDefinition<TEntity> filter, UpdateDefinition<TEntity> update, FindOneAndUpdateOptions<TEntity> options, CancellationToken cToken);
+        Task<TEntity> Delete(FilterDefinition<TEntity> filter, FindOneAndDeleteOptions<TEntity> options, CancellationToken cToken);
 
 
         Task<IEnumerable<TEntity>> FindMany(FilterDefinition<TEntity> filter, FindOptions options, CancellationToken cToken);
-        Task<bool> InsertMany(IEnumerable<TEntity> entities, InsertOneOptions options, CancellationToken cToken);
+        Task<bool> InsertMany(IEnumerable<TEntity> entities, InsertManyOptions options, CancellationToken cToken);
         Task<UpdateResult> UpdateMany(FilterDefinition<TEntity> filter, UpdateDefinition<TEntity> update, UpdateOptions options, CancellationToken cToken);
-        Task<bool> DeleteMany(FilterDefinition<TEntity> filter, DeleteOptions options, CancellationToken cToken);
+        Task<DeleteResult> DeleteMany(FilterDefinition<TEntity> filter, DeleteOptions options, CancellationToken cToken);
     }
 }
--- DbService/Repositories/BaseRepository.cs
+++ DbService/Repositories/BaseRepository.cs
@@ -1,4 +1,5 @@
 ﻿using DbService.Interfaces;
+using Microsoft.VisualBasic;
 using MongoDB.Driver;
 using ILogger = Serilog.ILogger;
 
@@ -24,46 +25,141 @@ namespace DbService.Repositories
 
         public virtual async Task<TEntity> Find(FilterDefinition<TEntity> filter, FindOptions options, CancellationToken cToken)
         {
-            var collection = InitCollection();
+            try
+            {
+                var collection = InitCollection();
+                var result = await collection
+                    .Find(filter, options)
+                    .FirstOrDefaultAsync(cToken);
+                return result;
+            }
+            catch(Exception err)
+            {
+                _logger.Error(err, $"Find error. Details: {err.Message}");
+            }
 
-            var result = await collection.Find(filter, options).FirstOrDefaultAsync(cToken);
-
-            return result;
+            return null!;
         }
 
-        public virtual Task<IEnumerable<TEntity>> FindMany(FilterDefinition<TEntity> filter, FindOptions options, CancellationToken cToken)
+        public virtual async Task<IEnumerable<TEntity>> FindMany(FilterDefinition<TEntity> filter, FindOptions options, CancellationToken cToken)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var collection = InitCollection();
+                var result = await collection
+                    .Find(filter, options)
+                    .ToListAsync(cToken);
+
+                return result;
+            }
+            catch (Exception err)
+            {
+                _logger.Error(err, $"FindMany error. Details: {err.Message}");
+            }
+
+            return null!;
         }
 
-        public virtual Task<TEntity> Insert(TEntity entity, InsertOneOptions options, CancellationToken cToken)
+        public virtual async Task<bool> Insert(TEntity entity, InsertOneOptions options, CancellationToken cToken)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var collection = InitCollection();
+                await collection.InsertOneAsync(entity, options, cToken);
+
+                return true;
+            }
+            catch (Exception err)
+            {
+                _logger.Error(err, $"Insert error. Details: {err.Message}");
+            }
+
+            return false;
         }
 
-        public virtual Task<bool> InsertMany(IEnumerable<TEntity> entities, InsertOneOptions options, CancellationToken cToken)
+        public virtual async Task<bool> InsertMany(IEnumerable<TEntity> entities, InsertManyOptions options, CancellationToken cToken)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var collection = InitCollection();
+                await collection.InsertManyAsync(entities, options, cToken);
+
+                return true;
+            }
+            catch (Exception err)
+            {
+                _logger.Error(err, $"InsertMany error. Details: {err.Message}");
+            }
+
+            return false;
         }
 
-        public virtual Task<TEntity> Update(FilterDefinition<TEntity> filter, UpdateDefinition<TEntity> update, UpdateOptions options, CancellationToken cToken)
+        public virtual async Task<TEntity> Update(FilterDefinition<TEntity> filter, UpdateDefinition<TEntity> update, FindOneAndUpdateOptions<TEntity> options, CancellationToken cToken)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var collection = InitCollection();
+                var result = await collection.FindOneAndUpdateAsync(filter, update, options, cToken);
+
+                return result;
+            }
+            catch (Exception err)
+            {
+                _logger.Error(err, $"Update error. Details: {err.Message}");
+            }
+
+            return null!;
         }
 
-        public virtual Task<UpdateResult> UpdateMany(FilterDefinition<TEntity> filter, UpdateDefinition<TEntity> update, UpdateOptions options, CancellationToken cToken)
+        public virtual async Task<UpdateResult> UpdateMany(FilterDefinition<TEntity> filter, UpdateDefinition<TEntity> update, UpdateOptions options, CancellationToken cToken)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var collection = InitCollection();
+                var result = await collection.UpdateManyAsync(filter, update, options, cToken);
+
+                return result;
+            }
+            catch (Exception err)
+            {
+                _logger.Error(err, $"UpdateMany error. Details: {err.Message}");
+            }
+
+            return null!;
         }
 
-        public virtual Task<TEntity> Delete(FilterDefinition<TEntity> filter, DeleteOptions options, CancellationToken cToken)
+        public virtual async Task<TEntity> Delete(FilterDefinition<TEntity> filter, FindOneAndDeleteOptions<TEntity> options, CancellationToken cToken)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var collection = InitCollection();
+                var result = await collection.FindOneAndDeleteAsync(filter, options, cToken);
+
+                return result;
+            }
+            catch (Exception err)
+            {
+                _logger.Error(err, $"Insert error. Details: {err.Message}");
+            }
+
+            return null!;
         }
 
-        public virtual Task<bool> DeleteMany(FilterDefinition<TEntity> filter, DeleteOptions options, CancellationToken cToken)
+        public virtual async Task<DeleteResult> DeleteMany(FilterDefinition<TEntity> filter, DeleteOptions options, CancellationToken cToken)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var collection = InitCollection();
+                var result = await collection.DeleteManyAsync(filter, options, cToken);
+
+                return result;
+            }
+            catch (Exception err)
+            {
+                _logger.Error(err, $"Insert error. Details: {err.Message}");
+            }
+
+            return null!;
         }
 
         protected IMongoCollection<TEntity> InitCollection()
